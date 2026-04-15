9d1c751b84d258867f466aed66d046243116d5dc
ExpertosTI <44707155+ExpertosTI@users.noreply.github.com>
2026-04-15 14:44:07 -0400

    fix: database path in seed script for docker container

--- scratch/seed_variety.js
+++ scratch/seed_variety.js
@@ -1,6 +1,6 @@
 const Database = require('better-sqlite3');
 const path = require('path');
-const db = new Database(path.join(__dirname, 'data', 'wedding.db'));
+const db = new Database(path.join(__dirname, '..', 'data', 'wedding.db'));
 
 const weddings = [
     {
